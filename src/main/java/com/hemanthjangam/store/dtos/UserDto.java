e235908d92b0be5472be36a83b6b42a1a73c5a8e
HemanthJangam <hemanthjangam.edu@gmail.com>
2025-08-16 20:37:45 +0530

    code updated

--- src/main/java/com/hemanthjangam/store/dtos/UserDto.java
+++ src/main/java/com/hemanthjangam/store/dtos/UserDto.java
@@ -1,9 +1,13 @@
 package com.hemanthjangam.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
--- src/main/java/com/hemanthjangam/store/mappers/UserMapper.java
+++ src/main/java/com/hemanthjangam/store/mappers/UserMapper.java
@@ -3,6 +3,7 @@ package com.hemanthjangam.store.mappers;
 import com.hemanthjangam.store.dtos.UserDto;
 import com.hemanthjangam.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
