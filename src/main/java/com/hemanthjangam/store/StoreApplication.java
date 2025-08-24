3e258da29406d3219baa833802a9d414b3e98843
HemanthJangam <hemanthjangam.edu@gmail.com>
2025-08-24 19:41:53 +0530

    repositories added

--- src/main/java/com/hemanthjangam/store/StoreApplication.java
+++ src/main/java/com/hemanthjangam/store/StoreApplication.java
@@ -1,5 +1,7 @@
 package com.hemanthjangam.store;
 
+import com.hemanthjangam.store.entities.User;
+import com.hemanthjangam.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,7 +11,15 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context =  SpringApplication.run(StoreApplication.class, args);
+        var repository =  context.getBean(UserRepository.class);
 
+        var user = User.builder()
+                .name("Bunny")
+                .email("bunny@example.com")
+                .password("password")
+                .build();
+
+        repository.save(user);
 	}
 
 }
